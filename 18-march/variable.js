432ed58eca39752d6b62eb4746484837d5ad1527
abhisheksuresh <abhisheksuresh2017@gmail.com>
2023-03-19 05:07:57 -0700

    biodata using variable

--- 18-march/variable.js
+++
@@ -1,9 +0,0 @@
-var FirstName = "John"
-var SecondName = "Doc"
-var MyNameIs = FirstName + " " + SecondName
-var BornYear = 1996
-const CurrentYear = 2023
-var MyAgeIs =CurrentYear - BornYear
-var IStudyIn = 5
-
-console .log("My name is" + " " + MyNameIs + ". " + "My age is " + MyAgeIs + ". " + "Im studying in" +" " +  IStudyIn +  "th grade.");
---
+++ 19-march/biod.js
@@ -0,0 +1,24 @@
+var FirstName = "Abhishek"
+var SecondName = "Suresh"
+var FullName = FirstName + " "+ SecondName
+var BirthDate = 09
+var BirthMonth = "May"
+var BirthYear = 1999
+const CurrentYear = 2023
+var Age = CurrentYear - BirthYear
+var  CourseEnrolled = "JavaScript"
+var District = "Malappuram"
+var State = "Kerala"
+var Hobby1 = "Singing"
+var Hobby2 = "Dancing"
+var HObby3 ="chatting"
+
+console.log(`${FullName}`)
+console.log(`${BirthYear}-${BirthMonth}-${BirthDate}`)
+console.log(`My name is ${FullName}.`)
+console.log(`I'm ${Age} years old and I'm currently enrolled in ${CourseEnrolled}.`)
+console.log(`I'm coming from ${District} district of ${State}.`)
+console.log(`My hobbies are ${Hobby1},${Hobby2},${HObby3}`)
+console.log(`Thankyou`)
+
+
