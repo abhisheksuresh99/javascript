2a76306c7421bac6d07b5723b43f62abae1b5305
abhisheksuresh <abhisheksuresh2017@gmail.com>
2023-03-19 20:25:34 +0530

    learned about arithematic operations

---
+++ 19-march/operators/arithmeticOperators.js
@@ -0,0 +1,63 @@
+//............Number Addition.................
+{
+    
+    let Y = 7
+    let x = 8
+    let Result = x + Y
+
+
+    console.log("Result;",Result);
+}
+
+
+//............String Addition......
+{
+
+   let FirstName = "Abhishek"; 
+   let WhiteSpace = " ";
+   let SecondName = "suresh";
+   let result = FirstName + WhiteSpace + SecondName;
+
+   console.log("string Adding Result;", result)
+
+}
+
+
+
+//..........String and Number Addition......
+
+{
+
+        let String ="Abhishek Suresh";
+        let number = 1999;
+        let result = String + number;
+
+        console.log("String and Number Addition;",result)
+
+}
+
+//............Boolean Addition...........
+
+{
+
+      let booleanTrue = true;
+      let booleanFalse = false
+      let result = booleanTrue + booleanFalse
+
+      console.log("boolean and Number Addition;",result)
+}
+
+
+//.................Boolean and String Addition...........
+
+{
+
+       let booleanTrue =true;
+       let string = "Abhishek"
+       let result =string + booleanTrue;
+
+       console.log("booleanTrue + string addition;",result)
+
+
+
+}
